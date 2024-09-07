33fdab719bd5e541e3c3549a82974d14a389da86
Pedroscsilva <pedroscsilva2001@gmail.com>
2024-09-07 12:56:45 +0200

    feat adding a diagram click logic

--- app/dashboard/app.py
+++ app/dashboard/app.py
@@ -8,11 +8,16 @@ import numpy as np
 import io
 import base64
 import json
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 
 
 car_list = pd.read_csv('./data/car_list.csv')
 detected_flaws = pd.read_csv('./data/detected_flaws.csv', index_col='index')
 flaw_cause = pd.read_csv('./data/flaw_cause.csv')
+annotation_path = '../data_visualization/crop_diagram_data.json'
+with open(annotation_path, 'r') as f:
+    annotations = json.load(f)
 
 explained_detected_flaws = detected_flaws.merge(flaw_cause, how='left', on='flaw_type')
 complete_df = explained_detected_flaws.merge(car_list, how='left', on='car_id')
@@ -23,10 +28,7 @@ data = complete_df.sort_values(by='arrived_at')
 
 def create_flaw_diagram(filtered_df, filename):
     img_path = '../data_visualization/crop_diagram_data.png'
-    annotation_path = '../data_visualization/crop_diagram_data.json'
     img = Image.open(img_path)
-    with open(annotation_path, 'r') as f:
-        annotations = json.load(f)
 
     draw = ImageDraw.Draw(img, 'RGBA')
     flaw_data = filtered_df.groupby('roi')['flaw_type'].count()
@@ -80,10 +82,19 @@ def create_flaw_diagram(filtered_df, filename):
 
     return dcc.Graph(
             figure=fig,
-            config={'displayModeBar': True}
+            config={'displayModeBar': True},
+            id='inner_diagram'
         )
 
-app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
+def find_polygon(x, y):
+    point = Point(x, y)
+    for shape in annotations['shapes']:
+        polygon = Polygon(shape['points'])
+        if polygon.contains(point):
+            return shape['label']
+    return
+
+app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
 
 app.layout = dbc.Container([
     dbc.Row([
@@ -147,15 +158,64 @@ app.layout = dbc.Container([
             html.Div(id='flaw-diagram'),
             dcc.Graph(id='flaws-by-type'),
         ], width=5),
-    ])
+    ]),
+    dcc.Interval(
+        id='load_interval',
+        n_intervals=0,
+        max_intervals=0,
+        interval=1
+    )
 ], className='mx-0 my-0', fluid=True)
 
 @app.callback(
-    [
+    [   
         Output("flaws-by-color", "figure"),
         Output("flaws-by-cause", "figure"),
         Output("flaws-by-type", "figure"),
-        Output("flaw-diagram", 'children'),
+        Output("flaw-diagram", 'children')
+    ],
+    [
+        Input('load_interval', 'n_intervals')
+    ],
+    prevent_initial_call=False
+)
+def init_dash(click):
+    flaws_by_color = px.bar(
+        data.groupby('color').size().reset_index(name='flaws'),
+        x='flaws',
+        y='color',
+        orientation='h',
+        title='Flaws by Color'
+    )
+
+    # Plot: Flaws by cause (horizontal bar chart)
+    flaws_by_cause = px.bar(
+        data.groupby('cause').size().reset_index(name='flaws'),
+        x='flaws',
+        y='cause',
+        orientation='h',
+        title='Flaws by Cause'
+    )
+
+    # Plot: Amount of flaws by flaw type (horizontal bar chart)
+    flaws_by_type = px.bar(
+        data.groupby('flaw_type').size().reset_index(name='flaws'),
+        x='flaws',
+        y='flaw_type',
+        orientation='h',
+        title='Flaws by Type'
+    ).update_yaxes(autorange='reversed')
+
+    diagram_chart = create_flaw_diagram(data, 'lala')
+
+    return flaws_by_color, flaws_by_cause, flaws_by_type, diagram_chart
+
+@app.callback(
+    [
+        Output("flaws-by-color", "figure", allow_duplicate=True),
+        Output("flaws-by-cause", "figure", allow_duplicate=True),
+        Output("flaws-by-type", "figure", allow_duplicate=True),
+        Output("flaw-diagram", 'children', allow_duplicate=True),
         Output('clean-filter-btn', 'n_clicks')
     ],
     [
@@ -166,10 +226,12 @@ app.layout = dbc.Container([
         Input('flaws-by-cause', 'clickData'),
         Input('flaws-by-color', 'clickData'),
         Input('flaws-by-type', 'clickData'),
+        Input('inner_diagram', 'clickData'),
         Input('clean-filter-btn', 'n_clicks')
-    ]
+    ],
+    prevent_initial_call=True
 )
-def update_plots(selected_colors, selected_models, start_date, end_date, click_cause, click_color, click_type, clean_filter):
+def update_plots(selected_colors, selected_models, start_date, end_date, click_cause, click_color, click_type, click_diagram, clean_filter):
     filtered_df = data
     if selected_colors and clean_filter == 0:
         filtered_df = filtered_df[filtered_df['color'].isin(selected_colors)]
@@ -194,6 +256,12 @@ def update_plots(selected_colors, selected_models, start_date, end_date, click_c
     if click_type and clean_filter == 0:
         label = [click_type['points'][0]['label']]
         filtered_df = filtered_df[filtered_df['flaw_type'].isin(label)]
+    
+    if click_diagram and clean_filter == 0:
+        x = click_diagram['points'][0]['x']
+        y = click_diagram['points'][0]['y']
+        label = [find_polygon(x, y)]
+        filtered_df = filtered_df[filtered_df['roi'].isin(label)]
     
     n_clicks = 0
 
