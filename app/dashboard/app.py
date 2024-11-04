dcda2aa097725e0d88632db3bee34c63b8736906
Pedroscsilva <pedroscsilva2001@gmail.com>
2024-11-04 14:43:03 +0100

    feat: improvements on diagram mapping, visualization and filtering

--- app/dashboard/app.py
+++ app/dashboard/app.py
@@ -1,4 +1,5 @@
 from dash import Dash, html, dcc, Input, Output
+from dash.dependencies import State
 import plotly.express as px
 import dash_bootstrap_components as dbc
 import pandas as pd
@@ -11,7 +12,7 @@ import json
 from shapely.geometry import Point
 from shapely.geometry.polygon import Polygon
 
-
+# load dfs and annotation data
 car_list = pd.read_csv('./data/car_list.csv')
 detected_flaws = pd.read_csv('./data/detected_flaws.csv', index_col='index')
 flaw_cause = pd.read_csv('./data/flaw_cause.csv')
@@ -19,15 +20,17 @@ annotation_path = './data/crop_diagram_data.json'
 with open(annotation_path, 'r') as f:
     annotations = json.load(f)
 
+# configure final dfs
 explained_detected_flaws = detected_flaws.merge(flaw_cause, how='left', on='flaw_type')
 complete_df = explained_detected_flaws.merge(car_list, how='left', on='car_id')
-
 complete_df['arrived_at'] = pd.to_datetime(complete_df['arrived_at'], dayfirst=True)
-
 data = complete_df.sort_values(by='arrived_at')
+
+# set global variables
 primary_text_color = '#000000'
 secondary_text_color = 'rgba(0, 0, 0, 0.65)'
 
+# create the main diagram on the dashboard
 def create_flaw_diagram(filtered_df, filename):
     img_path = './assets/crop_diagram_data.png'
     img = Image.open(img_path)
@@ -35,6 +38,8 @@ def create_flaw_diagram(filtered_df, filename):
     draw = ImageDraw.Draw(img, 'RGBA')
     flaw_data = filtered_df.groupby('roi')['flaw_type'].count()
     max_flaws = max(flaw_data) if not flaw_data.empty else 1
+
+    print(flaw_data)
 
     def get_color(flaw_count):
         color_ratio = 255 / max_flaws
@@ -45,7 +50,7 @@ def create_flaw_diagram(filtered_df, filename):
         x_coords = [p[0] for p in polygon]
         y_coords = [p[1] for p in polygon]
         centroid_x = sum(x_coords) / len(polygon)
-        centroid_y = sum(y_coords) / len(polygon) - 10
+        centroid_y = sum(y_coords) / len(polygon) - 15
         return (centroid_x, centroid_y)
 
     font = ImageFont.load_default(size=20)
@@ -57,7 +62,7 @@ def create_flaw_diagram(filtered_df, filename):
             color = get_color(flaw_data[part_name])
             draw.polygon(polygon, fill=color)
             centroid = get_centroid(polygon)
-            draw.text(centroid, str(flaw_data[part_name]), fill='white', font=font)
+            draw.text(centroid, str(flaw_data[part_name]), fill='white', font=font, stroke_width=2, stroke_fill='black')
 
     img_array = np.array(img)
     fig = px.imshow(img_array, title='Amount of Flaws per Car Part')
@@ -307,7 +312,12 @@ def init_dash(click):
         Output("flaws-by-type", "figure", allow_duplicate=True),
         Output("flaw-diagram", 'children', allow_duplicate=True),
         Output('clean-filter-btn', 'n_clicks'),
-        Output('flaws-by-date', 'figure', allow_duplicate=True)
+        Output('flaws-by-date', 'figure', allow_duplicate=True),
+        Output('flaws-by-cause', 'clickData'),
+        Output('flaws-by-color', 'clickData'),
+        Output('flaws-by-type', 'clickData'),
+        Output('inner_diagram', 'clickData'),
+        Output('flaws-by-date', 'clickData')
     ],
     [
         Input("color-checkboxes", "value"),
@@ -318,41 +328,35 @@ def init_dash(click):
         Input('flaws-by-color', 'clickData'),
         Input('flaws-by-type', 'clickData'),
         Input('inner_diagram', 'clickData'),
-        Input('clean-filter-btn', 'n_clicks'),
-        Input('flaws-by-date', 'clickData')
+        Input('flaws-by-date', 'clickData'),
+        Input('clean-filter-btn', 'n_clicks')
     ],
+    [State('clean-filter-btn', 'n_clicks')],
     prevent_initial_call=True
 )
-def update_plots(selected_colors, selected_models, start_date, end_date, click_cause, click_color, click_type, click_diagram, clean_filter, click_flaw_date):
-    filtered_df = data
-    if selected_colors and clean_filter == 0:
-        filtered_df = filtered_df[filtered_df['color'].isin(selected_colors)]
-
-    if selected_models and clean_filter == 0:
-        filtered_df = filtered_df[filtered_df['model'].isin(selected_models)]
-
-    if start_date and end_date and clean_filter == 0:
-        filtered_df = filtered_df[
-            (filtered_df['arrived_at'] >= start_date) & (filtered_df['arrived_at'] <= end_date)
-        ]
+def update_plots(selected_colors, selected_models, start_date, end_date, click_cause, click_color, click_type, click_diagram, click_flaw_date, clean_filter, clean_filter_state):
+    filtered_df = data.copy()
     
-    if click_cause and clean_filter == 0:
+    if clean_filter and clean_filter != clean_filter_state:
+        n_clicks = 0
+        return generate_plots(data) + (n_clicks,) + (None, None, None, None, None)
+
+    if selected_colors:
+        filtered_df = filtered_df[filtered_df['color'].isin(selected_colors)]
+    if selected_models:
+        filtered_df = filtered_df[filtered_df['model'].isin(selected_models)]
+    if start_date and end_date:
+        filtered_df = filtered_df[(filtered_df['arrived_at'] >= start_date) & (filtered_df['arrived_at'] <= end_date)]
+    if click_cause:
         label = [click_cause['points'][0]['label']]
         filtered_df = filtered_df[filtered_df['cause'].isin(label)]
-
-    if click_color and clean_filter == 0:
+    if click_color:
         label = [click_color['points'][0]['label']]
         filtered_df = filtered_df[filtered_df['color'].isin(label)]
-    
-    if click_type and clean_filter == 0:
+    if click_type:
         label = [click_type['points'][0]['label']]
         filtered_df = filtered_df[filtered_df['flaw_type'].isin(label)]
-    
-    # if click_flaw_date and clean_filter == 0:
-    #     label = [click_flaw_date['points'][0]['x']]
-    #     filtered_df = filtered_df[filtered_df['arrived_at'].isin(label)]
-    
-    if click_diagram and clean_filter == 0:
+    if click_diagram:
         x = click_diagram['points'][0]['x']
         y = click_diagram['points'][0]['y']
         label = find_polygon(x, y)
@@ -363,7 +367,8 @@ def update_plots(selected_colors, selected_models, start_date, end_date, click_c
 
     flaws_by_color, flaws_by_cause, flaws_by_type, diagram_chart, flaws_by_time = generate_plots(filtered_df)
 
-    return flaws_by_color, flaws_by_cause, flaws_by_type, diagram_chart, n_clicks, flaws_by_time
+    return flaws_by_color, flaws_by_cause, flaws_by_type, diagram_chart, n_clicks, flaws_by_time, None, None, None, None, None
+
 
 if __name__ == "__main__":
     app.run_server(debug=True)
